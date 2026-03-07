2989816464703e671bb07ffc34bc7a459557055f
Nguyễn Tất Kiên <tatkien06082005@gmail.com>
2026-03-07 19:01:57 +0700

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders YoloHome navbar brand', () => {
-  render(<App />);
-  const brandElements = screen.getAllByText(/YoloHome/i);
-  expect(brandElements.length).toBeGreaterThan(0);
-});
